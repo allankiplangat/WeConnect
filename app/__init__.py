511e442b1d2d3533e598bba4cb3643bea693da2d
allankiplangat <allankiplangat22@outlook.com>
2018-03-07 15:52:17 +0300

    [Feature #155546588] Add business registration endpoint

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-import config
-from flask import Flask
-
-app = Flask(__name__)
-app.config.from_object(config.config_environments['development'])
-app.config['SECRET_KEY'] = "allan2327,."
---
+++ models/business.py
@@ -0,0 +1,24 @@
+
+class BusinessList(object):
+    def __init__(self):
+        self.businesses_details = []
+
+    def add_business(self, _id, name, mobile_number, description, category, location ):
+        businesses_detail= {
+            "_id":_id,
+            "name":name,
+            "mobile_number": mobile_number,
+            "description":description,
+            "category": category,
+            "location": location,
+        }
+
+        self.businesses_details.append(businesses_detail)
+
+        test_input = False
+        for record in self.businesses_details:
+            if record.get("_id") == _id:
+                test_input = True
+        print(self.businesses_details)
+        if test_input:
+            return "Business added successfully"
